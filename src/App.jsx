472bc79cced4e11db12568ebb46ac53ee9febfde
abshar-hspl <Abshar.Fatima@harbingergroup.com>
2024-09-02 11:24:27 +0530

    Added user dashboard Changes in Login.jsx and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
 import ProductUpload from "./components/ProductUpload";
+import UserDashboard from "./pages/UserDashboard";
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route path="/user/dashboard" element={<UserDashboard />} />
           <Route path="/admin/dashboard/upload" element={<ProductUpload />} />
           {/* Add other routes here */}
         </Routes>
